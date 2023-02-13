dff6888fccdb12852ab4ac808750b07c16a91864
Brandon Cleveland <bcleveland1022@gmail.com>
2023-02-13 10:31:31 -0700

    added other clean and build scripts

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir_array=(
+"./service"
+"../web-app"
+"../plugins/arcgis"
+"../image/service"
+"../../nga-msi"
+"../random"
+)
+
+for d in "${dir_array[@]}";do
+    cd $d;
+    echo "Building $PWD"
+    npm ci
+    npm run build
+done
+
+cd ../../instance
+echo "npm i in $PWD"
+npm i --omit=dev ../service ../web-app/dist/app ../plugins/arcgis ../plugins/image/service ../plugins/nga-msi ../plugins/random

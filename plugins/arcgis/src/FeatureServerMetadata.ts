b16df0d94097d83c4c21209750c89a7f2caae4bb
Brandon Cleveland <bcleveland1022@gmail.com>
2023-02-16 17:26:21 -0700

    class that contains information about a feature layer

---
+++ plugins/arcgis/src/FeatureServerMetadata.ts
@@ -0,0 +1,23 @@
+/**
+ * Contains information about a specific arc feature layer.
+ */
+export class FeatureServerMetadata {
+
+    /**
+     * The url to the feature layer.
+     */
+    url: string;
+
+    /**
+     * The geometry type this feature layer accepts.
+     */
+    geometryType: string;
+
+    /**
+     * Constructor.
+     */
+    constructor() {
+        this.url = '';
+        this.geometryType = '';
+    }
+}

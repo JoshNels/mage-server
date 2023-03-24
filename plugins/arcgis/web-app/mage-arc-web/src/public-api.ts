e75306ea1ae3351a2ad1d242f11c229b888a2d2c
Brandon Cleveland <bcleveland1022@gmail.com>
2023-03-24 10:48:17 -0600

    initial start of a configuration UI

---
+++ plugins/arcgis/web-app/mage-arc-web/src/public-api.ts
@@ -0,0 +1,17 @@
+/*
+ * Public API Surface of mage-arc-web
+ */
+import { PluginHooks } from '@ngageoint/mage.web-core-lib/plugin'
+import { MageArcModule } from './lib/mage-arc.module'
+import { ArcAdminComponent } from './lib/arc-admin/arc-admin.component'
+
+export * from './lib/arc-admin/arc-admin.component'
+export * from './lib/mage-arc.module'
+
+export const MAGE_WEB_HOOKS: PluginHooks = {
+  module: MageArcModule,
+  adminTab: {
+    title: 'ArcGIS',
+    tabContentComponent: ArcAdminComponent
+  }
+}

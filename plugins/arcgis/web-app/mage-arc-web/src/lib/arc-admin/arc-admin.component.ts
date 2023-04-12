c8e3b66b6bfbae16b010d64eaa4643525698171e
Brandon Cleveland <bcleveland1022@gmail.com>
2023-04-12 08:34:48 -0600

    first stab at loading spinner

--- plugins/arcgis/web-app/mage-arc-web/src/lib/arc-admin/arc-admin.component.ts
+++ plugins/arcgis/web-app/mage-arc-web/src/lib/arc-admin/arc-admin.component.ts
@@ -17,6 +17,7 @@ export class ArcAdminComponent implements OnInit {
   private currentUrl: string;
   private timeoutId: number;
   layers: string[];
+  isLoading: boolean;
 
   arcLayerControl = new FormControl('', [Validators.required])
   @ViewChild('addLayerDialog', { static: true })
@@ -25,6 +26,7 @@ export class ArcAdminComponent implements OnInit {
   constructor(private arcService: ArcService, private dialog: MatDialog) {
     this.config = defaultArcGISPluginConfig;
     this.layers = new Array<string>();
+    this.isLoading = false;
     arcService.fetchArcConfig().subscribe(x => {
       this.config = x;
     })
@@ -50,11 +52,13 @@ export class ArcAdminComponent implements OnInit {
 
   fetchLayers(currentUrl: string) {
     console.log('Fetching layers for ' + currentUrl);
+    this.isLoading = true;
     this.arcService.fetchArcLayers(currentUrl).subscribe(x => {
       console.log('arclayer response ' + x);
       for(const layer of x.layers) {
         this.layers.push(layer.name);
       }
+      this.isLoading = false;
     })
   }
 
